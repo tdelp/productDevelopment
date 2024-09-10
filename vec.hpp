2b7643acd7f547f260bc6ee365aa8a885a60f294
Trenton Delp <tdelp@nd.edu>
2024-09-09 21:20:11 -0400

    Milestone 002 001

--- vec.hpp
+++ vec.hpp
@@ -5,22 +5,17 @@ template <typename T>
 class Tvec2
 {
 private:
-    T components[2]; // array to store x and y components
+    T components[2];
 
 public:
-    // References for direct access to components
     T& x, &y;
 
-    // Default constructor: initializes x and y to zero
     Tvec2() : components{0, 0}, x(components[0]), y(components[1]) {}
 
-    // Parameterized constructor: initializes x and y with given values
     Tvec2(T _x, T _y) : components{_x, _y}, x(components[0]), y(components[1]) {}
 
-    // Copy constructor: initializes x and y with values from another Tvec2 object
     Tvec2(const Tvec2& cp) : components{cp.x, cp.y}, x(components[0]), y(components[1]) {}
 
-    // Assignment operator: assigns values from another Tvec2 object
     Tvec2& operator=(const Tvec2& cp) {
         if (this != &cp) {
             x = cp.x;
