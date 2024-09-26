2946ba46d1f3e38f91d2a0ec0603b0367f8e8942
Trenton Delp <tdelp@nd.edu>
2024-09-26 12:56:20 -0400

    Milestone 003 008

--- sdl.cpp
+++ sdl.cpp
@@ -70,5 +70,55 @@ public:
 
 int main(int argc, char* args[])
 {
+    SDL_Window* window = nullptr;
+    SDL_Surface* vbuffer = nullptr;
+
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        return 1;
+    }
+
+    // Create window
+    window = SDL_CreateWindow("SDL2 Drawing", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, RES_X, RES_Y, SDL_WINDOW_SHOWN);
+    if (window == NULL) {
+        return 2;
+    }
+
+    // Get window surface
+    vbuffer = SDL_GetWindowSurface(window);
+    SDL_FillRect(vbuffer, NULL, SDL_MapRGB(vbuffer->format, 0x00, 0x00, 0x00)); // Clear screen to black
+    SDL_UpdateWindowSurface(window);
+
+    // Create Screen object
+    Screen screen(RES_X, RES_Y, vbuffer);
+
+    // Draw four lines from the center to the corners using Bresenham Line Algorithm
+    screen.drawLine(ivec2(RES_X / 2, RES_Y / 2), ivec2(0, 0), ivec3(255, 0, 0)); // Top-left
+    screen.drawLine(ivec2(RES_X / 2, RES_Y / 2), ivec2(RES_X, 0), ivec3(0, 255, 0)); // Top-right
+    screen.drawLine(ivec2(RES_X / 2, RES_Y / 2), ivec2(0, RES_Y), ivec3(0, 0, 255)); // Bottom-left
+    screen.drawLine(ivec2(RES_X / 2, RES_Y / 2), ivec2(RES_X, RES_Y), ivec3(255, 255, 0)); // Bottom-right
+
+    // Draw a box in the center of the screen
+    screen.drawSafeBox(ivec2(RES_X / 2 - 50, RES_Y / 2 - 50), ivec2(RES_X / 2 + 50, RES_Y / 2 + 50), ivec3(255, 0, 255)); // Centered box
+
+    // Update window with drawn content
+    screen.blitTo(vbuffer);
+    SDL_UpdateWindowSurface(window);
+
+    // Event loop
+    SDL_Event e;
+    bool quit = false;
+    while (!quit) {
+        while (SDL_PollEvent(&e)) {
+            if (e.type == SDL_QUIT)
+                quit = true;
+        }
+        SDL_UpdateWindowSurface(window);
+    }
+
+    // Destroy window and quit SDL
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
